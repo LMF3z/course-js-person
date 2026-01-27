c46ff84c3da4194e8ed64643077749b93b838910
LMF3z <moisesfreites3@gmail.com>
2026-01-26 21:43:40 -0400

    Ejemplo de variable por params en express

--- clase-03/src/routes/users/users.routes.ts
+++ clase-03/src/routes/users/users.routes.ts
@@ -6,6 +6,14 @@ router.get("/", (_, res) => {
     res.json({ message: "Usuarios route works!" })
 })
 
+router.get("/:id", (req, res) => {
+    const { id } = req.params
+
+    res.json({
+        message: `Usuario con el id: ${id}`
+    })
+})
+
 router.post("/", (req, res) => {
     const body = req.body
 
